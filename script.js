564a145abe53f3ce0be7758b151c96195c88f34e
Raja <srajasep27@gmail.com>
2023-07-06 21:15:21 +0530

    individual classes added for running and cycling

--- script.js
+++ script.js
@@ -77,5 +77,50 @@ class App{
     }
 }
 
-const app = new App();
+///////////////////////////////////////////////
+class Workout{
 
+    date = new Date();
+    id = (Date.now() + '').slice(-10);
+
+    constructor(coords,distance,duration){
+        this.coords = coords; // [lat, lng]
+        this.distance = distance; // km
+        this.duration = duration; // min
+    }
+}
+
+///////////////////////////////////////////////
+class Running extends Workout{
+    constructor(coords,distance,duration,cadence){
+        super(coords,distance,duration);
+        this.cadence = cadence;
+        this.calcPace();
+    }
+
+    calcPace(){
+        // min/km
+        this.pace = this.duration / this.distance;
+        return this.pace;
+    }
+}
+
+///////////////////////////////////////////////
+class Cycling extends Workout{
+    constructor(coords,distance,duration,elevationGain){
+        super(coords,distance,duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed()
+    }
+
+    calcSpeed(){
+        // km/hr
+        this.speed = this.distance / (this.duration/60);
+        return this.speed;
+    }
+}
+
+const app = new App();
+const run1 = new Running([39,-12],5.2,24,178);
+const cy1 = new Cycling([39,-12],27,95,523);
+console.log(run1,cy1)
